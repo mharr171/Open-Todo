872628b35e093aaafdf64733ebbca4cde466512c
Matthew Harris <mharr171.z@gmail.com>
2018-02-12 01:17:09 -0800

    created controllers, added create function

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
-  validates :title, presence: { message: "required"}
+  validates :title, presence: { message: "is required"}
+  validates :private, presence: { message: "is required"}
   belongs_to :user
   has_many :items
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, presence: { message: "is required"}
+  validates :password, presence: { message: "is required"}
+  validates :first_name, presence: { message: "is required"}
+  validates :last_name, presence: { message: "is required"}
+  
   has_many :lists
   has_many :items, through: :lists
 end
